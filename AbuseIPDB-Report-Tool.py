4ff9b84682dd11df137a62febfaa5c4f8e3927d5
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2021-12-18 00:36:31 -0500

    added support for pasting raw access logs
    use -l to paste an entire suspicious request, straight from an access log.

--- AbuseIPDB-Report-Tool.py
+++ AbuseIPDB-Report-Tool.py
@@ -1,31 +1,23 @@
 import sys
+import time
 import requests
+import re
 import json
 import pandas as pd
 
-#Globals
-apiKey = open("R:\Path\To\AbuseIPDBapiKey.txt", "r")
+#<---Globals--->
+apiKey = open("myapiKey.txt", "r")
 headers = {
     'Accept': 'application/json',
     'Key': apiKey.read()
 }
-df = pd.read_csv("\Categories.csv")
+
+#opens the .cvs of attack categories (referenced on https://www.abuseipdb.com/categories)
+df = pd.read_csv("Categories.csv")
 df_reset=df.set_index('#')
 
-#Report the suspicious IP. takes ip and comment variables returned from suspectIP()
-def reportIP(ip, comment, attackCategories):
-    url = 'https://api.abuseipdb.com/api/v2/report'
-    # String holding parameters to pass in json format
-    params = {
-        'ip': ip,
-        'categories': attackCategories,   #fix with db
-        'comment': comment
-        }
-    response = requests.request(method='POST', url=url, headers=headers, params=params)
-    # Formatted output
-    decodedResponse = json.loads(response.text)
-    jsondataReport = json.dumps(decodedResponse, sort_keys=True, indent=4)
-    print(jsondataReport)
+#<---functions--->
+#currently functions are not needed but used to build on top of
 
 #check / confirm the final report:
 def checkIP(ip):
@@ -42,6 +34,22 @@ def checkIP(ip):
     #prints out data
     print(jsondata)
 
+#Report the suspicious IP. takes ip, attackCategories, and comment variables returned from main() input.
+#Use after checkIP() to check for any http errors.
+def reportIP(ip, comment, attackCategories):
+    url = 'https://api.abuseipdb.com/api/v2/report'
+    # String holding parameters to pass in json format
+    params = {
+        'ip': ip,
+        'categories': attackCategories,
+        'comment': comment
+        }
+    response = requests.request(method='POST', url=url, headers=headers, params=params)
+    # Formatted output
+    decodedResponse = json.loads(response.text)
+    jsondataReport = json.dumps(decodedResponse, sort_keys=True, indent=4)
+    print(jsondataReport)
+
 #main function
 def main():
     # Get user input for analysis
@@ -51,17 +59,75 @@ def main():
     size = input('Enter Size:')
     client = input('Enter Client:')
     ref = input('Enter Refering URL:')
-    #Prints out the .csv file of Attack Categories
+    #Prints the .csv of Attack Categories
     print(df_reset)
     attackCategories = input('Enter Attack Categories That Apply (1,2,3..etc):')
     comment = 'IP:[' + ip + '] ' + 'Request:[' + request + '] ' + 'Time:[' + time + '] ' + 'Size:[' + size + '] ' + 'Client:[ ' + client + '] ' + 'Referring URL:[' + ref + '] '
     checkIP(ip)
+    #Ask user to confirm checkIP() has no errors/high abuse score.
     reportIPAddr = input('Report this IP? yes/no/exit:')
     if reportIPAddr == 'yes' or 'Yes' or 'y' or 'Y':
         reportIP(ip, comment, attackCategories)
     elif reportIPAddr == 'exit':
         sys.exit()
     else:
-        input('press control+c to leave')
+        exit = input('press control+c to leave')
 
-main()
+#Acess log function/-l. very similar to main() but much for efficiat for reporting IP's.
+def accessLog():
+    #User pastes raw access log in this input
+    accessLogPaste = input('Paste Access log: ')
+    #regex script that filters the raw access log for any range ip address.
+    regexScriptForIpAddress = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    #filtered ip address
+    ip = regexScriptForIpAddress.search(accessLogPaste)[0]
+    comment = accessLogPaste
+    checkIP(ip)
+    #Ask user to confrim checkIP() has no errors/high abuse score.
+    reportIPAddr = input('Report this IP? yes/no/exit:')
+    if reportIPAddr == 'yes' or 'Yes' or 'y' or 'Y':
+        #Prints the .csv of Attack Categories
+        print(df_reset)
+        attackCategories = input('Enter Attack Categories That Apply (1,2,3..etc):')
+        reportIP(ip, comment, attackCategories)
+        time.sleep(1)
+        exit = input('press control+c to leave')
+    elif reportIPAddr == 'exit':
+        sys.exit()
+    else:
+        print('Exiting in 30 seconds...')
+        time.sleep(30)
+        sys.exit()
+
+def menu():
+    print('''
+        _   _                 ___ ___ ___  ___     ___                   _      _____         _
+       /_\ | |__ _  _ ___ ___|_ _| _ \   \| _ )___| _ \___ _ __  ___ _ _| |_ __|_   _|__  ___| |
+      / _ \| '_ \ || (_-</ -_)| ||  _/ |) | _ \___|   / -_) '_ \/ _ \ '_|  _|___|| |/ _ \/ _ \ |
+     /_/ \_\_.__/\_,_/__/\___|___|_| |___/|___/   |_|_\___| .__/\___/_|  \__|    |_|\___/\___/_|
+                                                          |_|
+            - a simple python script to analyze/report suspicious IP's on AbuseIPDB -
+            * usage: AbuseIPDB-Report-Tool.py [args]
+            * Arguments: -help     *displays basic usage information.
+            *            -log      *use raw access log as input.
+
+
+    ''')
+
+#<---Start of Program--->
+
+#Trys to catch CLI arguments if they exist
+try:
+    if sys.argv[1] == '-h' or sys.argv[1] == '-help':
+        menu()
+        time.sleep(1)
+        main()
+    else:
+        sys.argv[1] =='-l' or sys.argv[1] =='-log'
+        accessLog()
+
+#Issue: if user selects 'no' on -l this except will be called and main() will start
+except:
+    menu()
+    time.sleep(1)
+    main()
